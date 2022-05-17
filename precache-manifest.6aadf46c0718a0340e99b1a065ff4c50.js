2ac588c7e2a795f991df849d85a09fb6703d5451
仲条高幸 <njn0te@NJ.local>
2022-05-18 03:20:54 +0900

    Add build folder

---
+++ precache-manifest.6aadf46c0718a0340e99b1a065ff4c50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f1a96fd4d54fd44a05c6ab9d73aead",
+    "url": "/national-data-explorer/index.html"
+  },
+  {
+    "revision": "27283bae57b3b0b68f58",
+    "url": "/national-data-explorer/static/css/main.042c54bf.chunk.css"
+  },
+  {
+    "revision": "9cd46ed9bbaf53565707",
+    "url": "/national-data-explorer/static/js/2.52c73644.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/national-data-explorer/static/js/2.52c73644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27283bae57b3b0b68f58",
+    "url": "/national-data-explorer/static/js/main.3e74de4e.chunk.js"
+  },
+  {
+    "revision": "cec1fad79c8050fba76b",
+    "url": "/national-data-explorer/static/js/runtime-main.e87a3ce9.js"
+  },
+  {
+    "revision": "5a69c256f0a91fe373bd2f13135cafbe",
+    "url": "/national-data-explorer/static/media/dot-grid.5a69c256.png"
+  }
+]);
