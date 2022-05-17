e19c1e201025e1719b0c7b1be483c760d638e37d
仲条高幸 <njn0te@NJ.local>
2022-05-18 03:41:19 +0900

    Change BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 import App from './app/app'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
